ffb057ab2d3d50c063791ad03704a40d0f4cab6f
derekhua <derekchristopherhua@gmail.com>
2016-02-27 01:44:37 -0800

    wolfram

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var server = require('http').Server(app);
 var bodyParser = require('body-parser')
 var Promise = require('promise');
 var twilio = require('twilio');
+var wolfram = require('wolfram').createClient("TKP9V8-V4KP6P5PKL");
 
 var accountSid = 'AC466b9301d8e9cefd4c841794be11d77a';
 var authToken = '430f808373847f0f46f9c7ed35ef3533';
@@ -23,11 +24,17 @@ app.use(function(req, res, next) {
 
 app.post('/',function(req, res) {
   var resp = new twilio.TwimlResponse();
-  resp.message('There we go');
-  res.writeHead(200, {
-    'Content-Type':'text/xml'
+
+  wolfram.query(req.body.Body, function(err, result) {
+    if(err) {
+      console.log(err);
+    }
+    resp.message("Result: " + result);    
+    res.writeHead(200, {
+      'Content-Type':'text/xml'
+    });
+    res.end(resp.toString());
   });
-  res.end(resp.toString());
 });
 
 server.listen(3000, function(){
